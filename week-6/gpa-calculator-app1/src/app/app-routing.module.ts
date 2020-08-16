2a24e962bbd70fb3bffffc44a192a1b7f75597ff
Clayton Stacy <clayton.stacy@gmail.com>
2020-08-16 11:20:18 -0500

    Finishing 6.3

---
+++ week-6/gpa-calculator-app1/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+
+const routes: Routes = [
+  {path: '', component: BaseLayoutComponent,
+  children: [
+    {
+      path: '',
+      component: HomeComponent
+    }
+  ]
+},
+{
+  path: 'session',
+  component: AuthLayoutComponent,
+  children: [
+    {
+      path: 'not-found',
+      component: NotFoundComponent
+    }
+  ]
+},
+{
+  path: '**',
+  redirectTo: 'session/not-found'
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
